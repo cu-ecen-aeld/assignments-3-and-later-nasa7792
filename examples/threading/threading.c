50448005d950e82deb84730020d439339faf28d1
Nalin <nalin@Nalin.myguest.virtualbox.org>
2025-02-07 11:42:56 -0700

    unit tests passing for assignment 4

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,7 +10,24 @@
 
 void* threadfunc(void* thread_param)
 {
-
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success=false;
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    int mutex_lock_ret_status=pthread_mutex_lock(thread_func_args->mutex);
+    if(mutex_lock_ret_status!=0){
+        ERROR_LOG("mutex lock failed ! %d",mutex_lock_ret_status);
+    }
+    else{
+       usleep(thread_func_args->wait_to_release_ms*1000); 
+       int mutex_release_stat=pthread_mutex_unlock(thread_func_args->mutex);
+       if(mutex_release_stat!=0){
+        ERROR_LOG("mutex release failed ! %d",mutex_release_stat);
+       }
+       else{
+        DEBUG_LOG("mutex lock released successfully!");
+        thread_func_args->thread_complete_success=true;
+       }
+    }
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
@@ -27,7 +44,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      * return true if successful.
      *
      * See implementation details in threading.h file comment block
-     */
-    return false;
+     */ 
+    struct thread_data *ptr_thread_data=(struct thread_data*)malloc(sizeof(struct thread_data));
+    ptr_thread_data->mutex=mutex;
+    ptr_thread_data->thread_complete_success=false;
+    ptr_thread_data->wait_to_obtain_ms=wait_to_obtain_ms;
+    ptr_thread_data->wait_to_release_ms=wait_to_release_ms;
+
+    bool thread_creation_stat=pthread_create(thread,NULL,threadfunc,ptr_thread_data);
+    if(thread_creation_stat!=0){
+        ERROR_LOG("failed to create thread");
+        return false;
+    }
+    DEBUG_LOG("thread created successfully");
+    return true;
 }
 
